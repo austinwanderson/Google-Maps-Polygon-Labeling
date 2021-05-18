3ffbff6b8feb4628feaaf1302e7719e29438d7dc
Austin Anderson <me@austinwayneanderson.com>
2021-05-18 13:04:27 -0700

    init commit

---
+++ polylabel.js
@@ -0,0 +1,14 @@
+/*
+ *
+ *   PolyLabel.js - A plugin for the Google Maps Javascript API to allow for labeling of polygons.
+ *
+ *   DEVELOPED BY - Austin Anderson
+ *                  me@austinwayneanderson.com
+ *                  austinwayneanderson.com
+ *
+ *   LATEST UPDATES:
+ * 
+ *     (5/18/2021): v0.1.0:
+ *			-Initial Build
+ *
+ */
